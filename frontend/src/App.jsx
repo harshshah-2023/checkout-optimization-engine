cc10a716d813c37a0148aeaa4e376a236b6541ef
Harsh Shah <harshshah9848@gmail.com>
2025-12-19 12:56:45 +0530

    chore: initialize payment gateway project structure and frontend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+
+  return (
+      <div className="min-h-screen bg-blue-200 text-white flex items-center justify-center">
+      <h1 className="text-2xl  bg-yellow-200 ">Tailwind Working</h1>
+    </div>
+   
+  )
+}
+
+export default App
